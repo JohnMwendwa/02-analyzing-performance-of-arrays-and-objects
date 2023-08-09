087a3fe29b72e6895a12edf8e3f0ab146c12c7ef
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-09 19:22:02 +0300

    initial commit

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// Big O of Arrays
+Insertion - "It depends...";
+Removal - "It depends...";
+Searching - O(N);
+Access - O(1);
+
+// Big O of Array Operations
+push - O(1)
+pop - O(1)
+shift - O(N)
+unshift - O(N)
+concat - O(N)
+slice - O(N)
+splice - O(N)
+sort - O(N * log N)
+forEach/map/filter/reduce/etc - O(N)
---
+++ objects.js
@@ -0,0 +1,17 @@
+let instructor = {
+  firstName: "John",
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3],
+};
+
+// Big O of Objects
+Insertion - O(1);
+Removal - O(1);
+Searching - O(N);
+Access - O(1);
+
+// Big O of Objects Methods
+Object.keys - O(N);
+Object.values - O(N);
+Object.entries - O(N);
+hasOwnProperty - O(1);
